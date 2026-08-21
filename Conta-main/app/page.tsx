5c18479867f2f3e65427606ec79122eb996eb126
lami216 <iiyad21694@gmail.com>
2026-08-21 21:28:16 +0000

    Delete Conta-main directory

--- Conta-main/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ContaApp from "./conta-app";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { SESSION_COOKIE, verifySession } from "../lib/auth";
-
-export default async function Home() {
-  if (!verifySession((await cookies()).get(SESSION_COOKIE)?.value)) redirect("/login");
-  return <ContaApp />;
-}
--- Conta-main/install.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [Parameter(Mandatory=$true)][string]$Target,
-  [switch]$ReplaceConfig,
-  [switch]$DryRun
-)
-$argsList = @($Target)
-if ($ReplaceConfig) { $argsList += "--replace-config" }
-if ($DryRun) { $argsList += "--dry-run" }
-python "$PSScriptRoot\install.py" @argsList
